da413d4fe4461a0de1b392996d4f79a1118bf2fd
Zarkeyson <djurdjev.zarko@gmail.com>
2022-02-10 09:29:01 +0100

    Set postgres as new database

--- myshop/settings.py
+++ myshop/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -84,8 +85,10 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myshop',
+        'USER': 'Zarko',
+        'PASSWORD': '123',
     }
 }
 
