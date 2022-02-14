d25270f144fb6791061605fcbf9c7414ff6fa218
Zarkeyson <djurdjev.zarko@gmail.com>
2022-02-14 14:47:23 +0100

    Added pagination, updated db

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.form import CartAddProductForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def product_list(request, category_slug=None):
     category = None
@@ -9,7 +10,28 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html', {'category': category,
+
+
+    all_products = Product.objects.all()
+
+    paginator = Paginator(all_products, 6)
+
+    page = request.GET.get('page')
+
+    p = paginator.get_page(page)
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(Paginator.num_pages)
+
+     
+    return render(request, 'shop/product/list.html', {
+                            'p': p,
+                            'products': page,
+                            'category': category,
                             'categories': categories, 
                             'products': products})
 
