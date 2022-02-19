69f11ca9f60e6ec2af00e7c3a8523e40eefdf114
Zarkeyson <djurdjev.zarko@gmail.com>
2022-02-19 12:46:55 +0100

    Added search template

--- shop/urls.py
+++ shop/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'), #ova dva koristimo u models.py fajlu kod funkcije get_absolute_url
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('search_products', views.search_products, name='search-products'),
 ]
